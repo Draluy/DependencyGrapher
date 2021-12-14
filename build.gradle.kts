06190e1bbc66171957edd2af0bf7bcfd575d6b3d
david <Kdavgit1002>
2021-12-14 22:11:40 +0100

    Removed compose from this project. WTF.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
-import org.jetbrains.compose.compose
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.21"
-    id("org.jetbrains.compose") version "1.0.0-alpha3"
 }
 
 group = "fr.raluy"
@@ -13,11 +10,9 @@ version = "1.0"
 repositories {
     google()
     mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
-    implementation(compose.desktop.currentOs)
     implementation(group= "net.sourceforge.plantuml", name= "plantuml", version= "8059")
     implementation("com.github.javaparser:javaparser-core:3.23.1")
     implementation("com.github.javaparser:javaparser-symbol-solver-core:3.23.1")
@@ -26,22 +21,15 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
-}
-
-compose.desktop {
-    application {
-        mainClass = "MainKt"
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "DependencyGrapher"
-            packageVersion = "1.0.0"
-        }
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
     }
 }
 
-
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
 
 tasks.test {
     useJUnitPlatform()
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,52 +1,25 @@
-import androidx.compose.desktop.DesktopTheme
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.awt.ComposeWindow
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
 import graphing.PlantUmlGrapher
-import java.awt.Desktop
+import java.awt.*
+
 import java.nio.file.Paths
 import javax.swing.JFileChooser
 
 private const val SELECT_THE_PROJECT_FOLDER = "Select the project folder"
 
-@Composable
-@Preview
-fun App() {
-    val text by remember { mutableStateOf(SELECT_THE_PROJECT_FOLDER) }
-    DesktopTheme {
-        Row(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
-            Button(onClick = {
-                val jFileChooser = JFileChooser()
-                jFileChooser.fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
-                jFileChooser.showDialog(ComposeWindow(), SELECT_THE_PROJECT_FOLDER)
+fun main()  {
+    val frame = Frame("Graph a project")
 
-                if(jFileChooser.selectedFile != null) {
-                    val absPath = jFileChooser.selectedFile.absolutePath
-                    val dependencies = ProjectDependencyAnalyzer().analyze(Paths.get(absPath))
-                    val indexUri = PlantUmlGrapher().createGraph(dependencies)
-                    Desktop.getDesktop().browse(indexUri);
-                }
-            }) {
-                Text(text)
-            }
-        }
-    }
-}
+    val button: Component = Button(SELECT_THE_PROJECT_FOLDER)
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    frame.add(button, BorderLayout.CENTER);
+    val jFileChooser = JFileChooser()
+    jFileChooser.fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
+    jFileChooser.showDialog(frame, SELECT_THE_PROJECT_FOLDER)
+
+    if(jFileChooser.selectedFile != null) {
+        val absPath = jFileChooser.selectedFile.absolutePath
+        val dependencies = ProjectDependencyAnalyzer().analyze(Paths.get(absPath))
+        val indexUri = PlantUmlGrapher().createGraph(dependencies)
+        Desktop.getDesktop().browse(indexUri);
     }
 }
